f9b6b9bd10d205876c714eeff152b1ac66cfb86e
Paul Curzon <p.curzon@qmul.ac.uk>
2020-06-26 21:13:50 +0100

    Add unit directory
    Organise into folders

--- unit1/helloworld.java
+++ unit1/helloworld.java
@@ -4,6 +4,14 @@ class hello
     public static void main (String [] param)
     {
        System.out.println("Hello");
+       hi("hi");
        System.exit(0);
     }
+
+    public static void hi (String message)
+    {
+       System.out.println(message);
+       return;
+    }
+
 }
